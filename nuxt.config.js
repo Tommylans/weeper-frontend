321fcc19df7f259b6e35d2f09af9a47c45e482d0
Seger van Vlijmen <seger.vanvlijmen@student.hu.nl>
2020-11-27 15:47:23 +0100

    Contact pagina V1 gamaakt

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,10 +14,11 @@ export default {
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
+    '@/assets/inputs.scss'
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
+  plugins: ['~/plugins/vue-formulate',
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,84 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        weeper
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+<FormulateForm
+  class="container"
+  v-model="formValues"
+>
+  <h2 class="form-title">Hoe kunnen wij u bereiken?</h2>
+  <div class="double-wide">
+    <div class="voornaam">
+  <FormulateInput
+    type="text"
+    name="Voornaam"
+    label="Voornaam"
+    placeholder="Voornaam"
+    error-behavior="live"
+  />
+    </div>
+    <div class="achternaam">
+  <FormulateInput
+    type="text"
+    name="Achternaam"
+    label="Achternaam"
+    placeholder="Achternaam"
+    error-behavior="live"
+  />
     </div>
   </div>
+    <FormulateInput
+      type="email"
+      name="Email"
+      label="E-mail adress"
+      placeholder="voorbeeld@example.nl"
+      validation="required|email"
+    />
+    <FormulateInput
+      type="tel"
+      name="Nummer"
+      label="Telefoon nummer"
+      placeholder="06-12345678"
+      validation="required"
+    />
+
+</FormulateForm>
 </template>
 
 <script>
-export default {}
+  export default {
+    data () {
+      return {
+        formValues: {}
+      }
+    }
+  }
 </script>
 
-<style>
+
+<style scoped>
 .container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+  padding: 2em;
+  border: 1px solid #a8a8a8;
+  border-radius: .5em;
+  max-width: 80%;
+  box-sizing: border-box;
 }
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.form-title {
+  margin-top: 0;
 }
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.login-form::v-deep .formulate-input .formulate-input-element {
+  max-width: none;
 }
-
-.links {
-  padding-top: 15px;
+@media (min-width: 420px) {
+  .double-wide {
+    display: flex;
+    width: 100%;
+  }
+}
+.voornaam {
+  margin-right: 5px;
+  width: 50%;
+}
+.achternaam {
+  margin-left: 5px;
+  width: 50%;
 }
 </style>
