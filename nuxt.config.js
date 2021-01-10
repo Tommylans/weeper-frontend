94539f96e383bd50e5ae1bdf5709acbc134bf880
Tom Lanser <tom.lanser@student.hu.nl>
2021-01-10 16:20:13 +0100

    moved font links

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,9 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Andika+New+Basic&display=swap" },
+      { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600&display=swap" },
     ]
   },
 
